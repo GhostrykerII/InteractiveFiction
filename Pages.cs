4364004599fccc80225c4fe2f25c8f79958ffbf2
GhostrykerII <tyler_kasper@hotmail.com>
2022-11-29 08:20:36 -0400

    Added pages

---
+++ Pages.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InteractiveFiction
+{
+    internal class Pages
+    {
+        public static void Page(int x)
+        {
+            switch (x)
+            {
+                case 0:
+                    if (Program.choiceA == true)
+                    {
+                        Program.page = 1;
+                        return;
+                    }
+                    else if (Program.choiceB == true)
+                    {
+                        Program.page = 2;
+                        return;
+                    }
+                    break;
+                case 1:
+                    if (Program.choiceA == true)
+                    {
+
+                    }
+                    else if (Program.choiceB == true)
+                    {
+
+                    }
+                    break;
+                case 2:
+                    if (Program.choiceA == true)
+                    {
+                        Program.page = 3;
+                        return;
+                    }
+                    else if (Program.choiceB == true)
+                    {
+                        Program.page = 1;
+                        return;
+                    }
+                    break;
+                case 3:
+                    if (Program.choiceA == true)
+                    {
+                        Program.page = 0;
+                        return;
+                    }
+                    else if (Program.choiceB == true)
+                    {
+                        Program.gameOver = true;
+                        return;
+                    }
+                    break;
+                case 4:
+                    if (Program.choiceA == true)
+                    {
+
+                    }
+                    else if (Program.choiceB == true)
+                    {
+
+                    }
+                    break;
+                case 5:
+                    if (Program.choiceA == true)
+                    {
+
+                    }
+                    else if (Program.choiceB == true)
+                    {
+
+                    }
+                    break;
+                case 6:
+                    if (Program.choiceA == true)
+                    {
+
+                    }
+                    else if (Program.choiceB == true)
+                    {
+
+                    }
+                    break;
+                case 7:
+                    if (Program.choiceA == true)
+                    {
+
+                    }
+                    else if (Program.choiceB == true)
+                    {
+
+                    }
+                    break;
+                case 8:
+                    if (Program.choiceA == true)
+                    {
+
+                    }
+                    else if (Program.choiceB == true)
+                    {
+
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
